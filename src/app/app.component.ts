e9e9aac2add75531b36685ec2a09242c2fc2620f
aaronksaunders <aaron@clearlyinnovative.com>
2017-07-21 01:03:33 -0400

    adding graphql

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Apollo, ApolloQueryObservable} from 'apollo-angular';
+import gql from 'graphql-tag';
+
+const AllUsers = gql `
+  query AllUsers {
+    allUsers {
+      id
+      first_name
+      last_name
+    }
+  }
+`;
+
+
+const addUser = gql`
+  mutation addUser($first_name:String!, $last_name:String!, $email:String!) {
+    addUser(first_name: $first_name, last_name: $last_name, email: $email) {
+      id
+    }
+  }
+`;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', 
+  templateUrl: './app.component.html', 
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  allUsers: ApolloQueryObservable<any>;
+
+  constructor(private apollo : Apollo) {}
+
+  ngOnInit() {
+    this.allUsers = this
+      .apollo
+      .watchQuery({query: AllUsers}) ;
+  }
+
+  addUserClicked(_data) {
+    console.log(_data.value)
+
+    this.apollo.mutate({
+      mutation: addUser,
+      variables : {
+        first_name : _data.value,
+        last_name : "Saunders",
+        email : "a@mail.com"
+      }
+    }).subscribe(({ data }) => {
+      console.log('got data', data);
+
+      this.allUsers.refetch();
+      
+    },(error) => {
+      console.log('there was an error sending the query', error);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
+import { ApolloModule } from 'apollo-angular';
+// by default, this client will send queries to `/graphql` (relative to the URL of your app)
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'https://aks-graphql-sample1.glitch.me/'
+  }),
+});
+
+export function provideClient(): ApolloClient {
+  return client;
+}
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ApolloModule.forRoot(provideClient)
   ],
   providers: [],
   bootstrap: [AppComponent]
